a027128f5c9b9cb8fda47858584ff0aa329830ae
paulayground <118198116+paulayground@users.noreply.github.com>
2026-03-08 20:01:54 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -1,12 +1,4 @@
-import re
-p = re.compile('[a-zA-Z]')
-
 def solution(my_string):
-    answer = 0
+    answer = sum(int(i) for i in my_string if i.isdigit())
     
-    for i in my_string:
-        if p.match(i) == None:
-            answer += int(i)
-            
-        
     return answer
