c27173c9f0f2790c151bc3904e19cbc318090e04
paulayground <118198116+paulayground@users.noreply.github.com>
2026-03-12 23:20:00 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.c
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 
 int main() {
-  int hour;
-  int minute;
-  int cooking_minute;
+  int hour, minute, cooking_minute;
   scanf("%d %d", &hour, &minute);
   scanf("%d", &cooking_minute);
 
-  int upper = 0;
   int total_minute = minute + cooking_minute;
-  if (total_minute >= 60) {
-    upper = (total_minute) / 60;
-    minute = total_minute % 60;
-  } else {
-    minute = total_minute;
-  }
 
-  hour = hour + upper;
-  if (hour >= 24) {
-    hour = hour % 24;
-  }
+  hour += total_minute / 60;
+  minute = total_minute % 60;
+
+  hour %= 24;
 
   printf("%d %d", hour, minute);
 
