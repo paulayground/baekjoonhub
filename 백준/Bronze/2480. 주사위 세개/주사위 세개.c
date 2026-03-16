8ba2cef9152629ffd14aa08172439da6fba9375a
paulayground <118198116+paulayground@users.noreply.github.com>
2026-03-16 19:22:52 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.c
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.c
@@ -5,21 +5,20 @@ int main() {
   int a, b, c, price;
   scanf("%d %d %d", &a, &b, &c);
 
-  if (a == b && b == c && a == c) {
+  // 3개가 같은 경우
+  if (a == b && b == c) {
     price = 10000 + a * 1000;
-  } else if (a != b && b != c && a != c) {
-    price = MAX(MAX(a, b), c) * 100;
-  } else {
-    if (a == b) {
-      price = 1000 + a * 100;
-    } else if (b == c) {
-      price = 1000 + b * 100;
-    } else {
-      price = 1000 + c * 100;
-    }
+  }
+  // 2개가 같은 경우
+  else if (a == b || a == c) {
+    price = 1000 + a * 100;
+  } else if (b == c) {
+    price = 1000 + b * 100;
+  }
+  // 전부 다른 경우
+  else {
+    price = MAX(a, MAX(b, c)) * 100;
   }
 
   printf("%d", price);
-
-  return 0;
 }
