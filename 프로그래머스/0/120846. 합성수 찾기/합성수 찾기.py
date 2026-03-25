9134d5837a7d0b03d989c92eb231a3b488f6c457
paulayground <118198116+paulayground@users.noreply.github.com>
2026-03-25 16:30:53 +0900

    [level 0] Title: 합성수 찾기, Time: 0.27 ms, Memory: 9.54 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -0,0 +1,22 @@
+from typing import List
+
+def solution(n):
+    nums: List[int] = []
+
+    for i in range(3, n + 1):
+        count = 0
+        for j in range(1, i + 1):
+            if i % j == 0:
+                count = count + 1
+
+            if count >= 3:
+                nums.append(i)
+                break
+
+    return len(nums)
+
+
+
+
+
+
